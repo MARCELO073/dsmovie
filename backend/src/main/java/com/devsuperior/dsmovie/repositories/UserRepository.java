6ee1d6fce794dc702cfef19067b31c54cc885ad4
MARCELO073 <marcelonogueira072@gmail.com>
2022-01-17 21:02:02 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.devsuperior.dsmovie.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.User;
 
-public interface UserRepository extends JpaRepository <Movie,Long> {
+public interface UserRepository extends JpaRepository <User,Long> {
+
+	User findByEmail(String email);
 
 }
